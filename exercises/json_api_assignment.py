bc91f77e449f08313c468b85caadc38fb84625eb
clbokea <clbo@kea.dk>
2019-11-03 13:36:02 +0100

    json

---
+++ exercises/json_api_assignment.py
@@ -0,0 +1,11 @@
+"""
+    JSON API Assignment
+    On https://github.com/python-elective-fall-2019/ there are some files with the file ending .ipynb.
+    These files are my (claus´) notes for the teachings, and they will cover what you should expect to be able to talk abourt at your exam. 
+    You should should download all these files and put them in one folder on your computer.
+
+    The way to do that is by 
+
+
+
+"""
